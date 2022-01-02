b7b45cbced5bd370da964b1e17fc6d44bb835de7
Iryna Shandar <iryna.shandar@gmail.com>
2022-01-02 18:42:09 +0200

    add js

--- src/App.js
+++ src/App.js
@@ -21,27 +21,22 @@ class App extends Component {
 
   formSubmitHandler = data => {
     const nextName = data.name.toLowerCase();
-    // проверка на совпадение с БД
     if (this.state.contacts.filter(item => item.name.toLowerCase() === nextName).length === 0) {
-      // заносим в БД новенького
       data.id = nanoid();
       const contact = { ...data };
       this.setState(({ contacts }) => ({
         contacts: [contact, ...contacts],
       }));
     } else {
-      // или ругаемся
       alert(data.name + ' is already in contacts');
     }
   };
 
   changeFilter = event => {
-    // вносим строку поиска в this.state.filter
     this.setState({ filter: event.currentTarget.value });
   };
 
   deleteContact = targetId => {
-    // создаем новый массив БД без элемента с выбранным ID (типа удаляем выбранный ID)
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== targetId),
     }));
@@ -50,7 +45,6 @@ class App extends Component {
   getVisibleContact = () => {
     const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
-    // ищем контакты в БД по имени из this.state.filter
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
   };
 
@@ -60,7 +54,6 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>goit-react-hw-02-phonebook</h1>
         <ContactForm onSubmit={this.formSubmitHandler} />
         <h2 className="title"> Contacts </h2>
         <Filter value={filter} onChange={this.changeFilter} />
